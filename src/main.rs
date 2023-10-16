a20f5c86aaac3495481281e0ecbdf222d390ef0d
Jihoon Kim <jihoon.kim@devsisters.com>
2023-10-16 16:27:06 +0900

    Add example program

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use lifetimes_and_closures::str_split_vec::str_split;
 
 fn main() {
-    let s = "a,bc,def";
-    let segments = read_delimiter_and_split(s);
+    let s = String::from("a,bc,def");
+    let segments = get_delimiter_and_split(&s);
     println!("{:?}", segments);
 }
 
-fn read_delimiter_and_split(s: &str) -> Vec<&str> {
+fn get_delimiter_and_split(s: &str) -> Vec<&str> {
     let delimiter = String::from(",");
     str_split(s, &delimiter)
 }
+
+// use lifetimes_and_closures::str_split_iterator::{str_split, StrSplit};
+
+// fn main() {
+//     let s = String::from("a,bc,def");
+//     let segments = get_delimiter_and_split(&s);
+//     println!("{:?}", segments.collect::<Vec<_>>());
+// }
+
+// fn get_delimiter_and_split(s: &str) -> StrSplit<'_, '_> {
+//     let delimiter = String::from(",");
+//     str_split(s, &delimiter)
+// }
